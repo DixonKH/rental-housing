9628fede8b2e4287fd7ddf8a72546a2466084501
DixonKH <khaydarovdilshod9@gmail.com>
2024-07-22 05:45:34 +0900

    fix: modify Chatting related design

--- libs/components/layout/LayoutBasic.tsx
+++ libs/components/layout/LayoutBasic.tsx
@@ -148,7 +148,7 @@ const withLayoutBasic = (Component: any) => {
 							<Component {...props} />
 						</Stack>
 
-						{user?._id && <Chat />}
+						{<Chat />}
 
 						<Stack id={'footer'}>
 							<Footer />
